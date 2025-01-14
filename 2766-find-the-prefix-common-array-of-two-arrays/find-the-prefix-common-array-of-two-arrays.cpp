dd52ad0f070c688eb1dd4852ffc91c511d015657
Harshit Bargujar <harshitrock111@gmail.com>
2025-01-14 23:59:51 +0530

    Time: 35 ms (6.70%) | Memory: 90.7 MB (16.41%) - LeetSync

---
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> prefixCommonArray(n);
+
+        // Initialize sets to store elements from A and B
+        unordered_set<int> elementsInA, elementsInB;
+
+        // Iterate through the elements of both arrays
+        for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
+            // Add current elements from A and B to respective sets
+            elementsInA.insert(A[currentIndex]);
+            elementsInB.insert(B[currentIndex]);
+
+            int commonCount = 0;
+
+            // Count common elements between the sets
+            for (int element : elementsInA) {
+                if (elementsInB.count(element)) {
+                    ++commonCount;
+                }
+            }
+
+            prefixCommonArray[currentIndex] = commonCount;
+        }
+        return prefixCommonArray;
+    }
+};
