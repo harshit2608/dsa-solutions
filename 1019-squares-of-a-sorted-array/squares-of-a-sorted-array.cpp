da906487bccd9894f0060395ef57d593a273b479
Harshit Bargujar <harshitrock111@gmail.com>
2025-04-04 00:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 30.1 MB (86.26%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,14 +1,25 @@
+const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
-
         vector<int> answer(n, 0);
-        for (int i = 0; i < n; i++) {
-            answer[i] = nums[i] * nums[i];
-        }
+        int i = 0, j = n - 1;
+        int k = n - 1;
 
-        sort(answer.begin(), answer.end());
+        while (i <= j) {
+            int sq1 = nums[i] * nums[i];
+            int sq2 = nums[j] * nums[j];
+
+            if (sq1 > sq2) {
+                answer[k--] = sq1;
+                i++;
+            } else {
+                answer[k--] = sq2;
+                j--;
+            }
+        }
         return answer;
     }
 };
