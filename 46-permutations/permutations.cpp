435c4bfbbe3598a90939b7e0c881aa75d039fb49
Harshit Bargujar <harshitrock111@gmail.com>
2025-05-01 23:09:33 +0530

    Time: 4 ms (14.76%) | Memory: 11 MB (32.43%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -3,28 +3,22 @@ const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
 class Solution {
 private:
     vector<vector<int>> answer;
-    void solve(vector<int>& nums, vector<int>& temp, vector<bool>& freq) {
-        if (temp.size() == nums.size()) {
-            answer.push_back(temp);
+    void solve(vector<int>& nums, int idx) {
+        if (idx == nums.size()) {
+            answer.push_back(nums);
             return;
         }
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (!freq[i]) {
-                freq[i] = true;
-                temp.push_back(nums[i]);
-                solve(nums, temp, freq);
-                temp.pop_back();
-                freq[i] = false;
-            }
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            solve(nums, idx + 1);
+            swap(nums[idx], nums[i]);
         }
     }
 
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> temp;
-        vector<bool> freq(nums.size(), false);
-        solve(nums, temp, freq);
+        solve(nums, 0);
         return answer;
     }
 };
