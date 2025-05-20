e781d5d62d76480205b2217b68a788371a6290c6
Harshit Bargujar <harshitrock111@gmail.com>
2025-05-21 01:29:14 +0530

    Time: 8 ms (61.57%) | Memory: 292.2 MB (98.14%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -5,23 +5,20 @@ public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         vector<int> subs(nums.size() + 1, 0);
         for (const auto& itr : queries) {
-            subs[itr[0]] += -1;
-            subs[itr[1] + 1] += 1;
+            subs[itr[0]] -= 1;
+            subs[itr[1] + 1]++;
         }
 
-        for (int i = 1; i < subs.size(); i++) {
-            subs[i] += subs[i - 1];
-        }
+        int prefix = 0;
 
         for (int i = 0; i < nums.size(); i++) {
-            nums[i] += subs[i];
-        }
-
-        for (const int& itr : nums) {
-            if (itr > 0) {
+            prefix += subs[i];
+            nums[i] += prefix;
+            if (nums[i] > 0) {
                 return false;
             }
         }
+
         return true;
     }
 };
