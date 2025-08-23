cca070f827e6898818bbb3f95e01f1e5fb4aac83
Harshit Bargujar <harshitrock111@gmail.com>
2025-08-24 02:37:38 +0530

    Time: 4 ms (13.41%) | Memory: 17.9 MB (5.64%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,11 +1,8 @@
-const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
-
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int answer = INT_MIN;
+        int prod = nums[0];
         int sz = nums.size();
-
         int prefix = 1, suffix = 1;
         for (int i = 0; i < sz; i++) {
             if (prefix == 0) {
@@ -17,10 +14,8 @@ public:
 
             prefix *= nums[i];
             suffix *= nums[sz - i - 1];
-
-            answer = max(answer, max(prefix, suffix));
+            prod = max({prod, prefix, suffix});
         }
-
-        return answer;
+        return prod;
     }
 };
