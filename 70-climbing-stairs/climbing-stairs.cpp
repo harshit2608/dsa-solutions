0c55e589df7aa164ffa22cd31148ef31c73a6cf5
Harshit Bargujar <harshitrock111@gmail.com>
2025-08-30 20:48:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.14%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,21 +2,28 @@ const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
 
 class Solution {
 private:
-    int recursion(vector<int>& dp, int n) {
-        if (n == 0)
+    int solve(int steps, vector<int>& dp) {
+        if (steps == 0) {
             return 1;
-        if (n < 0)
+        }
+        if (steps < 0) {
             return 0;
-        if (dp[n] != -1) {
-            return dp[n];
         }
 
-        return dp[n] = recursion(dp, n - 1) + recursion(dp, n - 2);
+        if (dp[steps] != -1) {
+            return dp[steps];
+        }
+        return dp[steps] = solve(steps - 1, dp) + solve(steps - 2, dp);
     }
 
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return recursion(dp, n);
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+
+        for (int step = 2; step <= n; step++) {
+            dp[step] = dp[step - 1] + dp[step - 2];
+        }
+        return dp[n];
     }
 };
