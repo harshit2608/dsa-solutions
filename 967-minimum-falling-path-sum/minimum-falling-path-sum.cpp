05f989f4e7ef3816c1dc7253a1c942b67874c71b
Harshit Bargujar <harshitrock111@gmail.com>
2025-09-07 01:44:22 +0530

    Time: 277 ms (5.21%) | Memory: 16.8 MB (6.21%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    vector<vector<int>> dp;
+    int solve(vector<vector<int>>& matrix, int n, int r, int c) {
+        if (r >= n || c >= n || c < 0) {
+            return 1e4;
+        }
+
+        if (r == n - 1) {
+            return matrix[r][c];
+        }
+
+        if (dp[r][c] != INT_MAX) {
+            return dp[r][c];
+        }
+
+        int leftdia = solve(matrix, n, r + 1, c - 1);
+        int down = solve(matrix, n, r + 1, c);
+        int rightdia = solve(matrix, n, r + 1, c + 1);
+
+        return dp[r][c] = matrix[r][c] + min({leftdia, down, rightdia});
+    }
+
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        int answer = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            dp.assign(n, vector<int>(n, INT_MAX));
+            answer = min(answer, solve(matrix, n, 0, i));
+        }
+
+        return answer;
+    }
+};
